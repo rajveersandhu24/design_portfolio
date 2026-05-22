880c38530dd935a4f8dcbdf4d693af334cfa2213
rajveersandhu24 <veerraja24@gmail.com>
2026-05-23 01:36:27 +0530

    warning rectified for more then 500kb file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/design_portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('matter-js')) {
+              return 'vendor-matter';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-motion';
+            }
+            return 'vendor'; // standard vendor chunk for React, Router, Lenis, etc.
+          }
+        }
+      }
+    }
+  }
 })
